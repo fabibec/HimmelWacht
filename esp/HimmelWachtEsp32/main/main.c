b4d16e3ffca6d8696b9e42ac2e1ad76c6e7b8ce5
Michael Specht <michael.maximilian.specht@gmail.com>
2025-05-27 21:26:59 +0200

    adapt main to test

--- esp/HimmelWachtEsp32/main/main.c
+++ esp/HimmelWachtEsp32/main/main.c
@@ -7,6 +7,7 @@
 #include "esp_wifi.h"
 
 #include "wifi-stack.h"
+#include "mqtt-stack.h"
 
 #include "freertos/task.h"
 
@@ -38,12 +39,22 @@ void app_main(void)
         ESP_LOG_BUFFER_CHAR("SSID", ap_info.ssid, sizeof(ap_info.ssid));
         ESP_LOGI(TAG, "Primary Channel: %d", ap_info.primary);
         ESP_LOGI(TAG, "RSSI: %d", ap_info.rssi);
-
-        ESP_LOGI(TAG, "Disconnecting in 5 seconds...");
-        vTaskDelay(pdMS_TO_TICKS(5000));
     }
 
-    ESP_ERROR_CHECK(wifi_stack_deinit());
+    ESP_LOGI(TAG, "Wi-Fi stack successfully initialized");
 
-    ESP_LOGI(TAG, "End of tutorial...");
+        // Configure MQTT component
+    mqtt_config_t mqtt_config = {
+        .broker_uri = "mqtt://192.168.178.33:1883",  // Replace with your broker IP
+        .topic = "vehicle/turret/cmd",               // Configurable topic
+        .client_id = "esp32_vehicle_01",             // Unique client ID
+        .keepalive = 60                              // Keep alive interval
+    };
+    
+    // Initialize MQTT component
+    ret = mqtt_stack_init(&mqtt_config);
+    if (ret != ESP_OK) {
+        ESP_LOGE("MAIN", "Failed to start MQTT component: %s", esp_err_to_name(ret));
+        return;
+    }
 }
